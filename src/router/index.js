b9e477da0ecad86f99f2072db268ca3d70deeffa
IkariShinji001 <duytran2003dev@gmail.com>
2024-08-10 10:07:35 +0700

    layout

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "Home",
-      component: () => import("../views/HomeView.vue"),
+      path: "/admin",
+      name: "admin",
+      component: () => import("../views/AdminLayout.vue"),
+      children: [
+        {
+          path: "tags",
+          component: () => import("../views/TagView.vue"),
+        },
+      ],
     },
   ],
 });
