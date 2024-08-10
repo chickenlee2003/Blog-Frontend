66bca0d33f288880166482395370961f5ecd6ae2
IkariShinji001 <duytran2003dev@gmail.com>
2024-08-10 10:28:02 +0700

    asd

--- src/views/TagView.vue
+++ src/views/TagView.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>asdasd</h1>
+  <section>
+    <h1>Quản lý Tags</h1>
+    <div class="input-search">
+      <q-input outlined label="Tìm kiếm theo tên">
+      </q-input>
+      <q-btn>Thêm Tag</q-btn>
+    </div>
+
+  </section>
 </template>
+
+<style scoped>
+  h1 {
+    font-size: 2rem;
+    margin: 10px;
+  }
+</style>
